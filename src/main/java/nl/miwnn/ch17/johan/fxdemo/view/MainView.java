31343becece82ccae6810ba08b89ed73240f709c
Simon van der Kooij <famvanderkooij@gmail.com>
2025-11-03 15:23:45 +0100

    Big fonts applied

--- src/main/java/nl/miwnn/ch17/johan/fxdemo/view/MainView.java
+++ src/main/java/nl/miwnn/ch17/johan/fxdemo/view/MainView.java
@@ -9,6 +9,8 @@ package nl.miwnn.ch17.johan.fxdemo.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import nl.miwnn.ch17.johan.fxdemo.viewmodel.MainViewModel;
 
 
@@ -16,11 +18,21 @@ public class MainView {
 
     @FXML private TextField nameField;
     @FXML private Label nameLabel;
+    @FXML private Label startLabel;
 
     private final MainViewModel viewModel = new MainViewModel();
 
     @FXML
     public void initialize() {
+        // Opmaak
+        nameField.setMaxWidth(600);
+        nameField.setFont(new Font("Arial", 30));
+        nameLabel.setFont(new Font("Arial", 30));
+        nameLabel.setTextFill(Color.RED);
+        startLabel.setFont(new Font("Arial", 30));
+
+
+
         // Binding tussen UI en ViewModel
         nameField.textProperty().bindBidirectional(viewModel.nameProperty());
         nameLabel.textProperty().bind(viewModel.displayedNameProperty());
